d559d46c3abe61b25f20aa2661489281e56a5edc
Marc Olivier ETOURNEAU <mo.etourneau@gmail.com>
2023-11-16 07:38:10 +0100

    Layout of images and text for each article

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,17 +2,20 @@
   <div class="ui main container">
     <ul>
       <li v-for="item in articles" :key="item.id">
-        <div class="ui message"  v-if="item.stockNumber != 0" :class="item.category.name == 'Nourriture' ? 'error' : 'success'">
-        {{ item.id }} / <h2>{{ item.name }}</h2> 
-        <br />
-        <span style="font-style: bold; font-size: large;">{{ item.shortDescription }}</span><br />
-        <span style="font-style: italic; font-size: small;">{{ item.longDescription }}</span><br />
-        <span style="text-decoration: underline;">Categorie : {{ item.category.name }}</span> <br />
-        ( {{ item.stockNumber }} en stock ) 
-        <button @click="decrement(item)">&nbsp;-&nbsp;</button>
-        <button @click="increment(item)">&nbsp;+&nbsp;</button>
-        <br />
-        <br />
+        <div class="menu-item ui message"  v-if="item.stockNumber != 0" :class="item.category.name == 'Nourriture' ? 'error' : 'success'">
+          <div class="left"><img class="menu-item__image" :src="'/images/articles/' + item.name + '.jpg'" />
+          </div>
+          <div class="right">
+          <h2>{{ item.id }} / {{ item.name }}</h2> 
+          <span style="font-style: bold; font-size: large;">{{ item.shortDescription }}</span><br />
+          <span style="font-style: italic; font-size: small;">{{ item.longDescription }}</span><br />
+          <span style="text-decoration: underline;">Categorie : {{ item.category.name }}</span> <br />
+          ( {{ item.stockNumber }} en stock ) 
+          <button @click="decrement(item)">&nbsp;-&nbsp;</button>
+          <button @click="increment(item)">&nbsp;+&nbsp;</button>
+          <br />
+          <br />
+        </div>
       </div>
     </li>
     </ul>
@@ -64,4 +67,15 @@ li {
 a {
   color: #42b983;
 }
+.menu-item {
+    display: flex;
+    width: 800px;
+    justify-content: space-between;
+    margin-bottom: 30px;
+  }
+  
+  .menu-item__image {
+    max-width: 200px;
+    vertical-align: auto;
+  }
 </style>
